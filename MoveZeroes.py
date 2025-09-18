4be90cef71ee1720d4a649cef7e14bd0d394eb30
arunsamy4444 <arunsamy4444@gmail.com>
2025-09-18 10:59:48 +0530

    Day 9

--- MoveZeroes.py
+++ MoveZeroes.py
@@ -1,22 +1,37 @@
+#------ SOL 1 ----- 
 def MoveZeroes(nums):
-    s = 0
+    l=0
     for i in range(len(nums)):
-        if nums[i] != 0:
-            nums[s],nums[i] = nums[i],nums[s]
-            s+=1
+        if nums[i] !=0:
+            nums[l],nums[i] = nums[i],nums[l]
+            l+=1
     print(nums)
-            # print(nums[i])
+      
+    
+print(MoveZeroes([0,1,0,3,0,12]))
+
+
+
+#------ SOL 2 ----- 
+# def MoveZeroes(nums):
+#     s = 0
+#     for i in range(len(nums)):
+#         if nums[i] != 0:
+#             nums[s],nums[i] = nums[i],nums[s]
+#             s+=1
+#     print(nums)
+#             # print(nums[i])
 
         
 
         
-    # nums2 = []
-    # for i in nums:
-    #     if i == 0:
-    #         nums2.append(i)
-    #         nums.remove(i)
-    # print(nums+nums2)
+#     # nums2 = []
+#     # for i in nums:
+#     #     if i == 0:
+#     #         nums2.append(i)
+#     #         nums.remove(i)
+#     # print(nums+nums2)
             
     
     
-print(MoveZeroes([0,1,0,3,0,12]))
+# print(MoveZeroes([0,1,0,3,0,12]))
